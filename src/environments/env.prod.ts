47bb5fe165e7bf6947d77107a831252ca5db46aa
Ziad Omar <zezobnomar55@gmail.com>
2026-09-11 00:26:24 +0300

    Update production HTTPS backend URL

--- src/environments/env.prod.ts
+++ src/environments/env.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:3000/api/v1/',
-  fileURL: 'http://localhost:3000/files/'
+  apiURL: 'https://giant-foxes-play.loca.lt/api/v1/',
+  fileURL: 'https://giant-foxes-play.loca.lt/files/'
 };
